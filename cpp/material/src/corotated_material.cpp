56899ac30f4aac54bc46626dff1a43791132e3c8
fly-pigTH <zylbhsf@126.com>
2025-04-14 16:05:40 +0800

    Task1

--- cpp/material/src/corotated_material.cpp
+++ cpp/material/src/corotated_material.cpp
@@ -13,7 +13,11 @@ const real CorotatedMaterial::Psi(const Matrix3r& F) const {
     ////////////////////////////////////////////////////////////////////////////
     //
     // TODO.
-    return 0;
+    const Matrix3r I = Matrix3r::Identity();
+    const Matrix3r e_c = S - I;
+    const real trace_e_c = e_c.trace();
+    // Task End
+    return mu() * e_c.array().square().sum() + lambda() / 2 * (trace_e_c * trace_e_c);
 }
 
 const Matrix3r CorotatedMaterial::P(const Matrix3r& F) const {
@@ -23,6 +27,24 @@ const Matrix3r CorotatedMaterial::P(const Matrix3r& F) const {
     //
     // TODO.
     return Matrix3r::Zero();
+     // Task Begin
+     Matrix3r R, S;
+     PolarDecomposition(F, R, S);
+     const auto dXdF = PolarDecompositionDifferential(F, R, S);
+     const Matrix9r dRdF= dXdF.first;
+     const Matrix9r dSdF = dXdF.second;
+     const Matrix3r I = Matrix3r::Identity();
+     const Matrix3r e_c = S - I;
+     const real trace_e_c = e_c.trace();
+     const Vector9r dPsidS = (2 * mu() * e_c + lambda() * trace_e_c * I).transpose().reshaped();
+     Matrix3r dPsidF = Matrix3r::Zero();
+ 
+     // using paper result directly
+     dPsidF = 2* mu() * (F-R) + lambda() * (S-I).trace() * R;
+     // self calculation
+     // dPsidF = (dSdF.transpose() * dPsidS).reshaped(3, 3).transpose();
+     // Task End
+     return dPsidF;
 }
 
 const Matrix3r CorotatedMaterial::dP(const Matrix3r& F,
@@ -47,7 +69,22 @@ const Matrix9r CorotatedMaterial::dPdF(const Matrix3r& F) const {
     ////////////////////////////////////////////////////////////////////////////
     //
     // TODO.
-    return Matrix9r::Zero();
+    // Task begin
+    Matrix3r R, S;
+    PolarDecomposition(F, R, S);
+    const Matrix9r I9 = Matrix9r::Identity();
+
+    const auto dXdF = PolarDecompositionDifferential(F, R, S);
+    const Matrix9r dRdF= dXdF.first;
+    const Matrix9r dSdF = dXdF.second;
+
+    Eigen::Matrix<double, 9, 1> vecdRdF = Eigen::Map<Eigen::Matrix<double, 9, 1>>(R.data());
+    Matrix9r RRT = vecdRdF * vecdRdF.transpose();
+
+    Matrix9r dPdF = Matrix9r::Zero();
+    dPdF = 2* mu() * (I9-dRdF) + lambda()*(R.transpose()*F).trace()*dRdF + lambda() * RRT;
+    //Task end
+    return dPdF;
 }
 
 }
